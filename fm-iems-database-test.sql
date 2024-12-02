fc50a1cc31256a29e7cacb8aae8354a61a58fea2
fabriziom90 <fabriziomastrobattista@gmail.com>
2024-12-02 21:47:25 +0100

    [FIX] Fixed dump database

--- fm-iems-database-test.sql
+++ fm-iems-database-test.sql
@@ -285,8 +285,6 @@ CREATE TABLE `users` (
 -- Dump dei dati per la tabella `users`
 --
 
-INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`) VALUES
-
 -- --------------------------------------------------------
 
 --
